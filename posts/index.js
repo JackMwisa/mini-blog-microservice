6ffd3ddd8f08880d1152e4486173a32680dc1f64
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2025-07-04 17:00:06 +0200

    Add post creation endpoint and body parsing
    Introduced body-parser middleware to handle JSON requests and implemented the /posts POST endpoint to create new posts with a title. Also updated package.json to add a start script using nodemon.

--- posts/index.js
+++ posts/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
+
 const app = express();
+app.use(bodyParser.json());
 
 const posts = {};
 
@@ -9,7 +12,15 @@ app.get('/posts', (req, res) => {
 });
 
 app.post('/posts', (req, res) => {
-    const id = randomBytes(4).toString('hex')
+  const id = randomBytes(4).toString('hex');
+  const { title } = req.body;
+
+  posts[id] = {
+    id,
+    title,
+  };
+
+  res.status(201).send(posts[id]);
 });
 
 app.listen(4000, () => {
